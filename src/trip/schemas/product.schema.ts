581f053750a12f517ee1f833a25a8267b28e0318
spolat <suatppolat@gmail.com>
2023-02-09 22:03:54 +0300

    trip schema added

---
+++ src/trip/schemas/product.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Schema as mSchema } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema({ _id: false, versionKey: false, timestamps: true })
+export class Product {
+  @Prop({
+    type: mSchema.Types.ObjectId,
+  })
+  categoryId: string;
+
+  @Prop({
+    type: mSchema.Types.Number,
+  })
+  count: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
