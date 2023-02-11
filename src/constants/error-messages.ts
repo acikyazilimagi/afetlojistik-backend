93f379fa28ac93bd7c4a0f8ca7043cdc5988218d
Ferhat Yalçın <ferhat.yalcin@getir.com>
2023-02-12 00:38:12 +0300

    [feat] update for adding validation exception

---
+++ src/constants/error-messages.ts
@@ -0,0 +1,17 @@
+export const ERROR_MESSAGES = {
+  UNKNOWN: {
+    error: 'UnknownError',
+    message: 'unknown error',
+    code: -1,
+  },
+  VALIDATION: {
+    error: 'ValidationError',
+    message: 'validation failed',
+    code: -2,
+  },
+  MICROSERVICE: {
+    error: 'MicroserviceError',
+    message: 'microservice error',
+    code: -3,
+  },
+};
--- src/user/exceptions/user-not-found.exception.ts
+++ src/user/exceptions/user-not-found.exception.ts
@@ -1,7 +1,12 @@
-import { NotFoundException } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import { TMSException } from 'src/common/exceptions/tms.exception';
 
-export default class UserNotFoundException extends NotFoundException {
+export default class UserNotFoundException extends TMSException {
   constructor() {
-    super('Kullanıcı adı veya doğrulama kodu hatalı!');
+    super(
+      'Kullanıcı adı veya doğrulama kodu hatalı!',
+      HttpStatus.NOT_FOUND,
+      HttpStatus.NOT_FOUND
+    );
   }
 }
