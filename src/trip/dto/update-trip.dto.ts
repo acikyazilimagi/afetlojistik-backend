8e9e882bf543b85a7cc22594520ddffb7bd15918
yldrmzffr <mail@muzaffer.dev>
2023-02-11 16:00:52 +0300

    (feat) Update ep method and new status change ep

--- src/trip/dto/update-trip.dto.ts
+++ src/trip/dto/update-trip.dto.ts
@@ -1,14 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateTripDto } from './create-trip.dto';
-import { IsNotEmpty, IsNumber } from 'class-validator';
-import { TripsStatuses } from '../types/trip.type';
 
-export class UpdateTripDto extends PartialType(CreateTripDto) {
-  @ApiProperty({
-    description: 'Status of the trip',
-    example: 200,
-  })
-  @IsNotEmpty()
-  @IsNumber()
-  status: TripsStatuses;
-}
+export class UpdateTripDto extends PartialType(CreateTripDto) {}
