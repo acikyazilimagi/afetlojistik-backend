f79e98c6381f79d80aa5a18c100a35a3b151d2c6
yldrmzffr <mail@muzaffer.dev>
2023-02-12 14:58:02 +0300

    (fix) sorting fixed for locations

--- src/location/logic/location.logic.ts
+++ src/location/logic/location.logic.ts
@@ -4,13 +4,7 @@ import { DisctrictDocument } from '../schemas/district.schema';
 export class LocationLogic {
   static sortCitiesAlphabetically(cities: CityDocument[]): CityDocument[] {
     return cities.sort(function (a, b) {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
+      return a.name.localeCompare(b.name, 'tr');
     });
   }
 
@@ -18,13 +12,7 @@ export class LocationLogic {
     districts: DisctrictDocument[]
   ): DisctrictDocument[] {
     return districts.sort(function (a, b) {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
+      return a.name.localeCompare(b.name, 'tr');
     });
   }
 }
