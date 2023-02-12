13c8f9ea9698a9df33370b63241a6024ba8374c7
spolat <suatppolat@gmail.com>
2023-02-12 16:42:57 +0300

    fix phone number transform

--- src/user/dto/login-user.dto.ts
+++ src/user/dto/login-user.dto.ts
@@ -1,10 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
 export class LoginUserDto {
   @IsString()
   @IsNotEmpty()
-  @Transform((value) => value.toString())
   @IsPhoneNumber('TR')
   @ApiProperty({
     description: 'Phone Number',
