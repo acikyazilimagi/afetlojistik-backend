70b04a62dc4b78c1e3fc4749ee6cd50eb4705585
Berkay Çavuş <berkay.cavus@getir.com>
2023-02-12 20:44:43 +0300

    remove header

--- src/integration/integration.controller.ts
+++ src/integration/integration.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Headers ,Param, UseGuards } from '@nestjs/common';
+import { Controller, Get ,Param, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AdminAuthGuard } from 'src/auth/admin.guard';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { TokenHeader } from 'src/common/headers/token.header';
 import { IntegrationService } from './integration.service';
 
 @ApiTags('Integration')
@@ -15,14 +14,13 @@ export class IntegrationController {
 
   @Get('')
   @ApiOperation({ summary: 'Get all integrators' })
-  getIntegrations(@Headers() tokenHeader: TokenHeader): Promise<any> {
+  getIntegrations(): Promise<any> {
     return this.integrationService.getIntegrations();
   }
 
   @Get(':integrationId')
   @ApiOperation({ summary: 'Get integration by integrationId' })
   getIntegrationById(
-    @Headers() tokenHeader: TokenHeader,
     @Param('integrationId') integrationId: string,
     ): Promise<any> {
     return this.integrationService.getIntegrationById(integrationId);
